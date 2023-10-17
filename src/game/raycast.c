8d42a18e30e3c529d7c45ee322a7377855b3f068
naterrie <naterrie@student.42lyon.fr>
2023-10-17 15:58:56 +0200

    adding d between x and border, angle and other things

--- src/game/raycast.c
+++ src/game/raycast.c
@@ -6,7 +6,7 @@
 /*   By: naterrie <naterrie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 16:31:47 by naterrie          #+#    #+#             */
-/*   Updated: 2023/10/16 15:35:20 by naterrie         ###   ########lyon.fr   */
+/*   Updated: 2023/10/17 15:54:38 by naterrie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,43 @@ double	d_coord(double x, double y, double xy, double yy)
 	return (sqrt(i * i + j * j));
 }
 
+double	closest_x(t_data *data, double x)
+{
+	int		tempx;
+	double	angle;
+	//int		posx;
+	double	ac;
+
+	angle = 180 + (data->player.angle * 180 / M_PI);
+	tempx = data->player.x;
+	if (x > 0)
+		tempx += 1;
+	ac = (data->player.x - tempx) / sin(180 - angle);
+	printf("bc = %f angle %f\n", ac, 180 - angle);
+	printf("angle = %f ", 180 - angle);
+	printf("d = %f\n\n", data->player.x - tempx);
+	return (data->player.x);
+}
+
+double	try_line(t_data *data, double x, double y)
+{
+	int	i;
+	int	j;
+
+	i = data->player.x;
+	j = data->player.y;
+	while (0)
+	{
+		if (x <= 0 || y <= 0 || x >= SCREEN_H || y >= SCREEN_W)
+			return (0);
+		((int *)data->mlx.addr)[(int)i * \
+			(data->line_length >> 2) + (int)j] = 0x0831838;
+		i += x;
+		j += y;
+	}
+	return (0);
+}
+
 double	draw_line(t_data *data, double x, double y)
 {
 	double	i;
@@ -55,11 +92,15 @@ double	draw_line(t_data *data, double x, double y)
 		i += x;
 		j += y;
 	}
-	return (d_coord(data->player.x, \
-		data->player.y, i / MAP_ZOOM, j / MAP_ZOOM));
+	//printf("%f %f\n", i, j);
+	return (d_coord(data->player.x * MAP_ZOOM, \
+		data->player.y * MAP_ZOOM, i, j));
 }
 
 void	raycast(t_data *data)
 {
-	printf("%f\n", draw_line(data, data->player.dir_x, data->player.dir_y));
+	closest_x(data, data->player.dir_x);
+	draw_line(data, data->player.dir_x, data->player.dir_y);
+	//printf("%f\n", try_line(data, data->player.dir_x, data->player.dir_y));
+
 }
