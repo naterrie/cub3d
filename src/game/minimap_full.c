a2bbb8f89c15e9098cb90161224d98b23d7e7e93
nibernar <nicober31320@hotmail.fr>
2023-11-21 23:20:43 +0100

    merge leaks

--- src/game/minimap_full.c
+++ src/game/minimap_full.c
@@ -15,27 +15,11 @@
 void	minimap_full(t_data *data)
 {
 	double	t;
-	//int		i;
-	//int		j;
 
-	//i = 0;
 	t = (double)FOV * (M_PI / 180);
 	data->player.angle_fov = (FOV * (M_PI / 180) / 2);
 	data->player.fov.x = cosf(t) * data->player.direction.x + -sinf(t) * data->player.direction.y;
 	data->player.fov.y = sinf(t) * data->player.direction.x + cosf(t) * data->player.direction.x;
-	// while (data->parsing.map[i])
-	// {
-	// 	j = 0;
-	// 	while (data->parsing.map[i][j])
-	// 	{
-	// 		if (data->parsing.map[i][j] == WALL)
-	// 			draw_square(data, i * MAP_ZOOM, j * MAP_ZOOM, 0xFFFF00);
-	// 		else if (data->parsing.map[i][j] == EMPTY)
-	// 			draw_square(data, i * MAP_ZOOM, j * MAP_ZOOM, 0x00FFFFFF);
-	// 		j++;
-	// 	}
-	// 	i++;
-	// }
 	display_game(data);
 	draw_minimap(data);
 	mlx_put_image_to_window(data->mlx.mlx, data->mlx.win, data->mlx.img, 0, 0);
